bedf6b62fdac93c6e59b087f857d15e129099612
Leandro Serra <leandro.serra.10@gmail.com>
2017-09-27 23:35:55 -0300

    Projeto realizado no curso de oficina Angular Rest Spring Boot da Algaworks

---
+++ lssd-patrimonio-api/src/main/java/com/lssdeveloper/patrimonio/LssdPatrimonioApiApplication.java
@@ -0,0 +1,12 @@
+package com.lssdeveloper.patrimonio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LssdPatrimonioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LssdPatrimonioApiApplication.class, args);
+	}
+}
---
+++ lssd-patrimonio-api/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into item (etiqueta, descricao, data_aquisicao) values ('NT0123', 'Notebook i7', sysdate());
+insert into item (etiqueta, descricao, data_aquisicao) values ('AU9875', 'Ford Fiesta', sysdate());
+insert into item (etiqueta, descricao, data_aquisicao) values ('NT0122', 'Notebook i7', sysdate());
+insert into item (etiqueta, descricao, data_aquisicao) values ('AU9876', 'Gol Geração 3', sysdate());
